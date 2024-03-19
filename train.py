7919f7d31c3e3649cdb36bfa087b67ee61798f08
Sahil Rajesh Shriwardhankar <sahiilshriwardhankar@gmail.com>
2024-03-20 00:50:10 +0530

    initial commit, and i also added todays work

---
+++ train.py
@@ -0,0 +1,25 @@
+import json
+from nltk_utils import tokenizer,stem,bag_of_words
+
+with open('intents.json','r') as f:
+    intents = json.load(f)
+
+all_words = []
+tags = []
+xy = []
+
+
+for intent in intents:
+    tag = intent['tag']
+    tags.append(tag)
+
+    for pattern in intent['patterns']:
+        w = tokenizer(pattern)
+        all_words.extend(w)
+        xy.append((w,tag))
+
+
+ignore_words = ['!','.',',']
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
